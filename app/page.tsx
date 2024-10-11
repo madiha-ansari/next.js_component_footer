8f0f1eae7d0129d829012522437f536d84ac2c14
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-11 19:46:11 +0500

    footers

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './footer/footer'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Page
